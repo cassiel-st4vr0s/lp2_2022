baecdae90f5d49dcf0431772cb4525d600a54701
cassiel-st4vr0s <111382100+cassiel-st4vr0s@users.noreply.github.com>
2022-08-31 15:06:14 -0400

    Produtor x consumidor

--- hello_world.c
+++ hello_world.c
@@ -1,8 +1,42 @@
-#include <stdio.h>
+from asyncio.windows_events import NULL
+from multiprocessing import Semaphore
+import threading
+import time
 
-int main(int argc,char** argv)
-{
-	printf("Hello World of Botafogo!\n");
+produto = []
+sem1=threading.Semaphore()
+sem2=threading.Semaphore()  
 
-	return 0;
-}
+def Produtor():
+    sem1.acquire()
+
+    global produto, produção
+    produção = True
+    
+       
+    for i in range(10):
+        produto.append(i) #aqui é realizado a produção
+        print('\nProduziu : {}'.format(produto))
+        time.sleep(1)
+                
+    sem1.release()
+
+
+def Consumidor():
+    sem2.acquire()
+    global produto, produção
+    
+
+   
+    while produção:
+        if (len(produto)==0):
+           break
+        x = produto.pop(0)  #aqui é realizado o consumo
+        print('\nConsumiu: {}'.format(x))
+        time.sleep(2)
+    sem2.release()
+       
+t1 = threading.Thread(target=Produtor)
+t2 = threading.Thread(target=Consumidor)
+t1.start()
+t2.start()
